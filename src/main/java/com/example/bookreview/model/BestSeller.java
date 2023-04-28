2ce7347b68ce5907cf0c7e6d0297201622a4a4f2
vnpugh <vnp19@yahoo.com>
2023-04-28 10:52:35 -0500

    adding getters & setters for bestseller

--- src/main/java/com/example/bookreview/model/BestSeller.java
+++ src/main/java/com/example/bookreview/model/BestSeller.java
@@ -57,7 +57,81 @@ public class BestSeller {
     public BestSeller() {
     }
 
-    
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Integer getWeeks() {
+        return weeks;
+    }
+
+    public void setWeeks(Integer weeks) {
+        this.weeks = weeks;
+    }
+
+    public Integer getSales() {
+        return sales;
+    }
+
+    public void setSales(Integer sales) {
+        this.sales = sales;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "BestSeller{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", rating=" + rating +
+                ", weeks=" + weeks +
+                ", sales=" + sales +
+                ", books=" + books +
+                '}';
+    }
 }
