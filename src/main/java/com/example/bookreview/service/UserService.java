bd50c5ceced8faf80d5bc332856c0e31006803b2
vnpugh <vnp19@yahoo.com>
2023-04-28 18:45:10 -0500

    modified UserService

--- src/main/java/com/example/bookreview/service/UserService.java
+++ src/main/java/com/example/bookreview/service/UserService.java
@@ -1,5 +1,9 @@
 package com.example.bookreview.service;
 
+import com.example.bookreview.exception.InformationExistException;
+import com.example.bookreview.model.User;
+import com.example.bookreview.repository.UserRepository;
+import com.example.bookreview.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +12,52 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private JWTUtils jwtUtils;
+    private AuthenticationManager authenticationManager;
+    private MyUserDetails myUserDetails;
 
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager, @Lazy MyUserDetails myUserDetails) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.myUserDetails = myUserDetails;
+    }
 
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
+                    " already exists");
+        }
+    }
 
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
 
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            myUserDetails = (MyUserDetails) ((org.springframework.security.core.Authentication) authentication).getPrincipal();
 
-
-
-
-
-
-
-
+            final String JWT = jwtUtils.generateJwtToken(myUserDetails);
+            return ResponseEntity.ok(new LoginResponse(JWT));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new LoginResponse("Error : username or password is incorrect"));
+        }
+    }
 }
