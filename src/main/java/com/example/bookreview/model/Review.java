8424f253aac062caabaf3c3e6e4133acd5dece20
vnpugh <vnp19@yahoo.com>
2023-04-28 09:10:02 -0500

    adding review model

---
+++ src/main/java/com/example/bookreview/model/Review.java
@@ -0,0 +1,123 @@
+package com.example.bookreview.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+    @Column
+    private LocalDate reviewDate; //using LocalDate to set the review date to the current date
+    @Column
+    private String title;
+    @Column
+    private String author;
+    @Column
+    private String genre;
+    @Column
+    private Double rating;
+    @Column
+    private String comment;
+
+    public Review(Long id, String username, LocalDate reviewDate, String title,
+                  String author, String genre, Double rating, String comment) {
+        this.id = id;
+        this.username = username;
+        this.reviewDate = reviewDate;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(LocalDate reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", reviewDate=" + reviewDate +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", rating=" + rating +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
