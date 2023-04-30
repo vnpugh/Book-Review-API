7b35a3e1421a9018712198a0bae9a831a3df25fc
vnpugh <vnp19@yahoo.com>
2023-04-30 00:27:22 -0500

    adding JWTUtils class

--- src/main/java/com/example/bookreview/exception/FailedToSaveReviewException.java
+++ src/main/java/com/example/bookreview/exception/FailedToSaveReviewException.java
@@ -1,5 +1,7 @@
 package com.example.bookreview.exception;
 
+
+//custom exception used to throw an exception if there is an error saving the review.
 public class FailedToSaveReviewException extends Throwable {
     public FailedToSaveReviewException(String message) {
 
---
+++ src/main/java/com/example/bookreview/service/JWTUtils.java
@@ -0,0 +1,4 @@
+package com.example.bookreview.service;
+
+public class JWTUtils {
+}
