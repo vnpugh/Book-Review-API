6065eb77443e50eccd87fa2a37bd1534b1f88ba7
vnpugh <vnp19@yahoo.com>
2023-04-30 01:10:19 -0500

    added endpoints to UserController

--- src/main/java/com/example/bookreview/controller/UserController.java
+++ src/main/java/com/example/bookreview/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.bookreview.controller;
 
 
 import com.example.bookreview.model.User;
+import com.example.bookreview.model.request.LoginRequest;
 import com.example.bookreview.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/auth/users")
 
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // http://localhost:9092/auth/users/register/
+    @PostMapping("/register/")
+    public User createUser(@RequestBody User userObject) { return userService.createUser(userObject);
+    }
+
+    // http://localhost:9092/auth/users/login/
+    //need to create a user endpoint for logging in
+    @PostMapping("/login/")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
+    }
+
+    
+
+
+
+
+
 
 
 }
