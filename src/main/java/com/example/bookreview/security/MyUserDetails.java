9a547903054d5714923901fba4069e4c8981e3f8
vnpugh <vnp19@yahoo.com>
2023-05-01 05:00:07 -0500

    adding docstring to myUserDetails

--- src/main/java/com/example/bookreview/security/MyUserDetails.java
+++ src/main/java/com/example/bookreview/security/MyUserDetails.java
@@ -2,15 +2,20 @@ package com.example.bookreview.security;
 
 
 import com.example.bookreview.model.User;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.HashSet;
 
 public class MyUserDetails implements UserDetails {
+
+    /**
+     * UserDetails is an interface that Spring Security uses for user authentication.
+     * this method is important because it allows you to access the user's details and custom
+       properties.
+     * This class will be used to create a UserDetails object that will be used by Spring Security.
+     * override the methods in the UserDetails interface.
+     */
 
     private final User user;
 
